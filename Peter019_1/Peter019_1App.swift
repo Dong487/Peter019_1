80404d08fb27aea3fd02c974aeba4787a56d6ee7
Dong487 <Dsheng7777777@gmail.com>
2022-08-14 14:10:14 +0800

    Initial Commit

---
+++ Peter019_1/Peter019_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Peter019_1App.swift
+//  Peter019_1
+//
+//  Created by DONG SHENG on 2022/8/14.
+//
+
+import SwiftUI
+
+@main
+struct Peter019_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
